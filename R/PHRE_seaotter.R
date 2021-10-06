d6242fee37832a9b4e8ab787ce0da3d2db644986
ZekeTuszynski <43019012+ZekeTuszynski@users.noreply.github.com>
2021-10-06 16:58:15 -0700

    post merge fixes

--- R/PHRE_seaotter.R
+++ R/PHRE_seaotter.R
@@ -8,6 +8,7 @@
 # This won't work for tuna. Why not?
 
 ## load data; specify file paths to supporting information
+<<<<<<< HEAD
 data <- read.csv("./data/F4_994_1346.csv")
 # data <- cbind(data$Xcoord, data$Ycoord)
 data <- cbind(data$TealeX, data$TealeY)
@@ -17,6 +18,16 @@ grid <- read.csv("./data/Tarjan&Tinker.2016.Data.csv")
 #grid is background environmental data (depth, distance, study site coordinates)
 data <- subset(grid, TealeY <= 160000 & TealeY > -180000, select = c("TealeX", "TealeY"))
 data <- data[sample(x = 1:nrow(data), size = 200), ] ## create random points in range of grid for locations
+=======
+data <- read.csv("./data/F4_994_1346.csv") # actual otter data
+data <- cbind(data$TealeX, data$TealeY) # cbind(data$Xcoord, data$Ycoord)
+# or use random points instead of actual data
+# data <- subset(grid, TealeY <= 160000 & TealeY > -180000, select = c("TealeX", "TealeY"))
+# data <- data[sample(x = 1:nrow(data), size = 200), ] ## create random points in range of grid for locations
+grid <- read.csv("./data/Tarjan&Tinker.2016.Data.csv")
+
+
+>>>>>>> 1c2ec8591b45225e2d47971eabf38345a6319a2c
 
 ## create list of rasters from the grid array
 library(raster)
@@ -50,14 +61,44 @@ HR <- phre(locs = data, rast = rasters, smooth = smoother, percent = 90)
 ## plot of phre list objects
 ## zoomed in on polygons
 plot(HR$Poly)
-plot(HR$array, add = T)
-#predictive surface: predicted otter abundance
-plot(HR$Polygon, col = "transparent", border = "red", add = T)
-#same as line 50 Polygon == poly
-points(HR$locs, pch = 20, cex = .1, col = "black")
-#actual otter sightings
+plot(HR$array, add = T) #predictive surface: predicted otter abundance
+plot(HR$Polygon, col = "transparent", border = "red", add = T) #same as line 50 Polygon == poly
+points(HR$locs, pch = 20, cex = .1, col = "black") #actual otter sightings
 
 
+
+
+# Generate coastline from data extents ####
+dir.create("./data/coastline")
+# install.packages("gbm.auto")
+# grids example until latlon grids file exists
+gridsexample <- data.frame(lat = c(36, 37), lon = c(-122.27, -121.74))
+
+coast <- gbm.auto::gbm.basemap( # IDK if you need to assign this to coast, think you might be able to just call it
+  bounds = c(),
+  grids = gridsexample, # name your grids database here: lat-lon transformed data powering 'array' above
+  gridslat = 1, # lat col number in grids
+  gridslon = 2, # lon col number in grids
+  getzip = TRUE,
+  zipvers = "2.3.7",
+  savedir = tempdir(),
+  # savename = "./data/coastline/Crop_Map",
+  savename = "/home/simon/Desktop/Crop_Map",
+  res = "CALC",
+  extrabounds = TRUE
+)
+
+# read in crop_map with:
+coast <- sf::st_read(dsn = "./data/coastline/Crop_Map.shp", layer = "Crop_Map", quiet = TRUE) # read in worldmap
+coast <- sf::st_read(dsn = "/home/simon/Desktop/Crop_Map.shp", layer = "Crop_Map", quiet = TRUE) # read in worldmap
+library(ggplot2)
+library(sf)
+library(ggspatial)
+ggplot2::ggplot() +
+  layer_spatial(coast, fill = "grey", lwd = 0)
+  # or add to existing plot with annotation_spatial() instead
+
+# IDK why crop_map isn't updating, is still north canada. maybe have to delete it each time? overwreite required?
 
 #add coastline next
 #x and y axes
