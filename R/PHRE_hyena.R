73f19f6fcb5378f854b6de02d7d1c64ad9771c05
Simon Dedman <simondedman@gmail.com>
2021-10-06 17:38:30 -0700

    SD hyena update

--- R/PHRE_hyena.R
+++ R/PHRE_hyena.R
@@ -12,7 +12,8 @@ library(maptools) ## required for spatialpoints
 library(raster)
 
 ## prepare inputs
-data <- read.csv("C:/Users/max/Desktop/Tarjan/hyena_data/OHB08.csv")
+# data <- read.csv("C:/Users/max/Desktop/Tarjan/hyena_data/OHB08.csv")
+data <- read.csv("/home/simon/Dropbox/Blocklab Monterey/Internships_Teaching_Recruitment/Zeke Tuszynski/Hyena_data_for_PHRE_test/OHB08.csv")
 # plot(data$LON, data$LAT)
 
 locs.ll <- subset(data, select = c("LON", "LAT"))
@@ -21,7 +22,9 @@ crs(locs.ll) <- CRS("+init=epsg:4326") ## assign a coordinate system
 locs.utm <- spTransform(locs.ll, CRSobj = CRS("+init=epsg:32733")) ## reproject
 
 fence <- rgdal::readOGR(dsn = "C:/Users/max/Desktop/Tarjan/hyena_data/Fences/Fences", layer = "Fences")
-out.fence.utm <- rgdal::readOGR(dsn = "C:/Users/max/Desktop/Tarjan/hyena_data/Fences/Fences", layer = "outside_fences_utm2")
+fence <- rgdal::readOGR(dsn = "/home/simon/Dropbox/Blocklab Monterey/Internships_Teaching_Recruitment/Zeke Tuszynski/Hyena_data_for_PHRE_test/Fences", layer = "Fences")
+# out.fence.utm <- rgdal::readOGR(dsn = "C:/Users/max/Desktop/Tarjan/hyena_data/Fences/Fences", layer = "outside_fences_utm2")
+out.fence.utm <- rgdal::readOGR(dsn = "/home/simon/Dropbox/Blocklab Monterey/Internships_Teaching_Recruitment/Zeke Tuszynski/Hyena_data_for_PHRE_test/Fences", layer = "outside_fences_utm2")
 
 ## reproject to projected coordinate system
 ## original - EPSG:4326 WGS 84.  convert- EPSG:32733 UTM 33S
@@ -59,13 +62,23 @@ rasters[[2]][] <- rasterToPoints(habitat)[, 2]
 rm(habitat, dd) ## remove habitat from workspace to save space
 
 ## run PHRE
+source("./R/phre_function.R")
 ## apply phre function
 HR <- phre(locs = locs.utm@coords,
            rast = rasters,
            smooth = "default",
            percent = 90,
            resolution = 1500)
+# from here####
+# error with their original data
+# Error in Polygons(HR.polys, ID = s) : srl not a list of Polygon objects
+# 3. stop("srl not a list of Polygon objects")
+# 2. Polygons(HR.polys, ID = s) at phre_function.R#108
+# 1. phre(locs = locs.utm@coords, rast = rasters, smooth = "default", percent = 90, resolution = 1500)
+
+
 # HR<-phre(locs=locs.utm@coords, rast=rasters)##resolution here is equal to the number of cells across the input raster, so a higher number leads to better resolution; default is 500
+# would be nice if phre function was multithreaded. Not sure if possible. One for issues.
 
 # gc() ##garbage collection to free up memory
 
