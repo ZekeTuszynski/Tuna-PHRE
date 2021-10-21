9bbd4324f6546505061303f6244208ccb01b8d5c
Simon Dedman <simondedman@gmail.com>
2021-10-20 17:56:00 -0700

    SD basemap fix attempts

--- R/sea otter ggplot 2.R
+++ R/sea otter ggplot 2.R
@@ -15,13 +15,17 @@ library(rgdal)
 library(ggplot2)
 library(dplyr)
 library(sf)
-
 library(maptools)
 library(ks)
 library(spex)
 library(rgeos)
 library(stars)
-
+library(magrittr)
+library(tidyverse)
+# Error: package or namespace load failed for ‘tidyverse’ in loadNamespace(j <- i[[1L]], c(lib.loc, .libPaths()), versionCheck = vI[[j]]):
+# there is no package called ‘reprex
+# zeke to fix####
+library(tidylog)
 
 
 ## load data; specify file paths to supporting information
@@ -66,18 +70,18 @@ otterarray_df <- as.data.frame(otterarray, xy = TRUE)
 
 # plain english notes about what every line is doing
 
-
 otterpoly <- st_as_sf(HR$Polygon)
 
-
+# in projected CRS
 ggplot() +
   geom_raster(data = otterarray_df , aes(x = x, y = y, fill = layer)) +
   scale_fill_viridis_c() +
   geom_point(data = locations_df , aes(x = V1, y = V2, colour = "otter locations")) +
-  geom_sf(data = otterpoly[[1]], colour="red", fill = NA) +
+  geom_sf(data = otterpoly[[1]], colour = "red", fill = NA) +
   ggtitle("Big Sur Sea Otter Home Range") + xlab("Longitude") + ylab("Latitude")
 
 
+# Convert to lat lon
 locationsll <- sf::st_as_sf(locations_df, coords = c("V1", "V2"), crs = 3311) %>%
   st_transform(4326)
 
@@ -87,39 +91,29 @@ otterarrayll <- sf::st_as_sf(otterarray_df, coords = c("x", "y"), crs = 3311) %>
 sf::st_crs(otterpoly) <- 3311
 otterpolyll <- sf::st_transform(otterpoly[[1]], 4326)
 
+hist(otterarrayll$layer) # loads of zeroes & NAs
 
-
-# from here####
-hist(otterarrayll$layer)
-library(tidyverse)
-# Error: package or namespace load failed for ‘tidyverse’ in loadNamespace(j <- i[[1L]], c(lib.loc, .libPaths()), versionCheck = vI[[j]]):
-# there is no package called ‘reprex
-# zeke to fix####
-library(tidylog)
-library(magrittr)
-
-tmp <- otterarrayll %>% # start with this object & save changes to it   otterarrayll %<>%
+otterarrayll %<>% # start with this object & save changes to it   otterarrayll %<>%
   drop_na(layer) %>% # remove NAs
   filter(layer > 0) # remove zeroes
 
-tmp <- otterarrayll[which(!is.na(otterarrayll$layer) | otterarrayll$layer == 0),]
-hist(expm1(tmp$layer))
-which(tmp$layer == 0)
-
+# in Lat Lon
 ggplot() +
-  geom_sf(data = tmp , aes(colour = layer)) + # otterarrayll # background surface gradient
+  geom_sf(data = otterarrayll , aes(colour = layer)) + # background surface gradient
   scale_colour_viridis_c() +
   geom_sf(data = locationsll, colour = "black", size = 1) + # otter points
-  geom_sf(data = otterpolyll, colour="red", fill = NA) + # red polygon outline
+  geom_sf(data = otterpolyll, colour = "red", fill = NA) + # red polygon outline
   ggtitle("Big Sur Sea Otter Home Range") + xlab("Longitude") + ylab("Latitude")
 
 
 
-
+# from here####
+# Add basemap
 
 
 # gbm.basemap####
 library(gbm.auto)
+
 # get lat lons from otterarrayll
 ottergeom <- otterarrayll %>%
   dplyr::mutate(lat = sf::st_coordinates(.)[,1],
@@ -127,22 +121,63 @@ ottergeom <- otterarrayll %>%
   dplyr::select(lat, lon) %>%
   sf::st_drop_geometry()
 
-
-
 st_write(ottergeom,
-"./data/Crop_Map.shp", driver = "ESRI Shapefile")
+         "./data/otterpoints.shp",
+         driver = "ESRI Shapefile")
 
 crop_map <- gbm.basemap(grids = ottergeom,
                         gridslat = 1,
-                        gridslon = 2)
-
-# trying URL 'https://www.ngdc.noaa.gov/mgg/shorelines/data/gshhg/latest/gshhg-shp-2.3.7.zip'
-# Content type 'application/zip' length 149157845 bytes (142.2 MB)
-# downloaded 142.2 MB
-#
+                        gridslon = 2,
+                        savedir = "../")
 # although coordinates are longitude/latitude, st_intersection assumes that they are planar
 # Writing layer `Crop_Map' to data source `Crop_Map.shp' using driver `ESRI Shapefile'
 # Writing 0 features with 6 fields and geometry type Unknown (any).
 # Error in `[<-`(`*tmp*`, record, 1, value = readBin(infile, integer(),  :
 #  subscript out of bounds
-# In addition: Warning message: attribute variables are assumed to be spatially constant throughout all geometrie
+# In addition: Warning message: attribute variables are assumed to be spatially constant throughout all geometries
+
+# > traceback()
+# 3: read.shx(paste(shape.name, ".shx", sep = ""))
+# 2: read.shapefile(savename)
+
+crop_map <- st_read(dsn = "../GSHHS_shp/CroppedMap/Crop_Map.shp", layer = "../GSHHS_shp/CroppedMap/Crop_Map")
+getwd() # "/home/simon/Dropbox/Blocklab Monterey/Internships_Teaching_Recruitment/Zeke Tuszynski/Tuna-PHRE"
+setwd("/home/simon/Dropbox/Blocklab Monterey/Internships_Teaching_Recruitment/Zeke Tuszynski/GSHHS_shp/CroppedMap/")
+crop_map <- st_read(dsn = "Crop_Map.shp", layer = "Crop_Map") # works. So there's a problem with file referencing above. Fix twice, basemap call & this.
+# Also fix basemap bounds issue.
+# crop_map:
+# Simple feature collection with 0 features and 6 fields
+# Bounding box:  xmin: NA ymin: NA xmax: NA ymax: NA
+# Geodetic CRS:  WGS 84
+
+setwd("/home/simon/Dropbox/Blocklab Monterey/Internships_Teaching_Recruitment/Zeke Tuszynski/GSHHS_shp/f/")
+world <- st_read(dsn = paste0("GSHHS_f_L1.shp"), layer = paste0("GSHHS_f_L1"), quiet = TRUE) # read in worldmap
+
+grids = ottergeom
+gridslat = 1
+gridslon = 2
+bounds <- c(range(grids[,gridslon]), range(grids[,gridslat])) #still required later despite sf/st update
+xmin = min(grids[,gridslon]) #for sf/st upgrade
+xmax = max(grids[,gridslon])
+ymin = min(grids[,gridslat])
+ymax = max(grids[,gridslat])
+
+sf::sf_use_s2(FALSE) # 2021 addition of s2 code to sf often causes: Error in s2_geography_from_wkb(x, oriented = oriented, check = check):
+# didn't help
+cropshp <- st_crop(world, xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax) # crop to extents
+# although coordinates are longitude/latitude, st_intersection assumes that they are planar
+# Warning message:
+#   attribute variables are assumed to be spatially constant throughout all geometries
+#
+# Simple feature collection with 0 features and 6 fields
+# Bounding box:  xmin: NA ymin: NA xmax: NA ymax: NA
+# Geodetic CRS:  WGS 84
+# [1] id         level      source     parent_id  sibling_id area       geometry
+# <0 rows> (or 0-length row.names)
+# Fails here. Debug, fix in gbm.basemap.
+
+
+setwd("/home/simon/Dropbox/Blocklab Monterey/Internships_Teaching_Recruitment/Zeke Tuszynski/GSHHS_shp/CroppedMap/")
+st_write(cropshp, dsn = "Crop_Map.shp")
+
+
